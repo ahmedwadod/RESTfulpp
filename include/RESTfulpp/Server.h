ec0d16fcf06aed3e180e72341229cf52219effc3
Ahmed Elkhalifa <ahmed@elkhalifa.dev>
2024-05-04 20:21:06 +0300

    Implemented more HTTP methods

--- include/RESTfulpp/Server.h
+++ include/RESTfulpp/Server.h
@@ -15,56 +15,64 @@
 #include <utility>
 #include <vector>
 
-namespace RESTfulpp {
+namespace RESTfulpp
+{
 
-typedef std::function<void(std::pair<sockpp::tcp_socket, sockpp::inet_address>)>
-    TCPClientHandler;
+  typedef std::function<void(std::pair<sockpp::tcp_socket, sockpp::inet_address>)>
+      TCPClientHandler;
 
-class ThreadPool {
-public:
-  ThreadPool();
-  ThreadPool(
-      std::function<void(std::pair<sockpp::tcp_socket, sockpp::inet_address>)>
-          runner_function);
-  void start(unsigned int num_threads);
-  void queue_job(std::pair<sockpp::tcp_socket, sockpp::inet_address> job);
-  void stop();
-  bool is_busy();
+  class ThreadPool
+  {
+  public:
+    ThreadPool();
+    ThreadPool(
+        std::function<void(std::pair<sockpp::tcp_socket, sockpp::inet_address>)>
+            runner_function);
+    void start(unsigned int num_threads);
+    void queue_job(std::pair<sockpp::tcp_socket, sockpp::inet_address> job);
+    void stop();
+    bool is_busy();
 
-private:
-  void _thread_loop();
+  private:
+    void _thread_loop();
 
-  std::function<void(std::pair<sockpp::tcp_socket, sockpp::inet_address>)>
-      runner;
-  bool should_terminate = false;
-  std::mutex queue_mutex;
-  std::condition_variable mutex_condition;
-  std::vector<std::thread> threads;
-  std::queue<std::pair<sockpp::tcp_socket, sockpp::inet_address>> jobs;
-};
+    std::function<void(std::pair<sockpp::tcp_socket, sockpp::inet_address>)>
+        runner;
+    bool should_terminate = false;
+    std::mutex queue_mutex;
+    std::condition_variable mutex_condition;
+    std::vector<std::thread> threads;
+    std::queue<std::pair<sockpp::tcp_socket, sockpp::inet_address>> jobs;
+  };
 
-// Server
-class Server {
-public:
-  Server(short port, unsigned int max_request_length = 1024);
-  ~Server();
+  // Server
+  class Server
+  {
+  public:
+    Server(short port, unsigned int max_request_length = 1024);
+    ~Server();
 
-  void any(std::string route_template, RouteHandler func);
-  void get(std::string route_template, RouteHandler func);
-  void post(std::string route_template, RouteHandler func);
-  void put(std::string route_template, RouteHandler func);
-  void Delete(std::string route_template, RouteHandler func);
-  void start(int thread_count = std::thread::hardware_concurrency());
+    void any(std::string route_template, RouteHandler func);
+    void get(std::string route_template, RouteHandler func);
+    void post(std::string route_template, RouteHandler func);
+    void put(std::string route_template, RouteHandler func);
+    void Delete(std::string route_template, RouteHandler func);
+    void patch(std::string route_template, RouteHandler func);
+    void head(std::string route_template, RouteHandler func);
+    void options(std::string route_template, RouteHandler func);
+    void trace(std::string route_template, RouteHandler func);
+    void connect(std::string route_template, RouteHandler func);
+    void start(int thread_count = std::thread::hardware_concurrency());
 
-private:
-  ThreadPool *_pool;
-  unsigned int _max_req_size;
-  TCPClientHandler tcpClientHandler;
-  sockpp::tcp_acceptor _acceptor;
-  void _route(std::string method, std::string route_template,
-              RouteHandler func);
-  std::vector<Internals::Router::RouteDefinition> _route_definitions;
-};
+  private:
+    ThreadPool *_pool;
+    unsigned int _max_req_size;
+    TCPClientHandler tcpClientHandler;
+    sockpp::tcp_acceptor _acceptor;
+    void _route(std::string method, std::string route_template,
+                RouteHandler func);
+    std::vector<Internals::Router::RouteDefinition> _route_definitions;
+  };
 } // namespace RESTfulpp
 
 #endif // !__RESTFULPP_SERVER_H__
--- src/Server.cpp
+++ src/Server.cpp
@@ -19,104 +19,148 @@
 using namespace RESTfulpp;
 
 Server::Server(short port, unsigned int max_request_length)
-    : _max_req_size(max_request_length) {
+    : _max_req_size(max_request_length)
+{
   sockpp::initialize();
   _acceptor = sockpp::tcp_acceptor(port);
-  if (!_acceptor) {
+  if (!_acceptor)
+  {
     std::cerr << "Error listening on port " << port << "\n";
-  } else {
+  }
+  else
+  {
     std::cout << "Server will listen on port " << port << "\n";
   }
 
   tcpClientHandler =
-      [&](std::pair<sockpp::tcp_socket, sockpp::inet_address> job) {
-        std::vector<char> req_buf(_max_req_size, 0);
-        size_t n = job.first.read(req_buf.data(), req_buf.size());
-        Request req;
-        try {
-          Internals::RequestParser parser;
-          req = parser.parse(req_buf.data(), n);
-        } catch (std::exception e) {
+      [&](std::pair<sockpp::tcp_socket, sockpp::inet_address> job)
+  {
+    std::vector<char> req_buf(_max_req_size, 0);
+    size_t n = job.first.read(req_buf.data(), req_buf.size());
+    Request req;
+    try
+    {
+      Internals::RequestParser parser;
+      req = parser.parse(req_buf.data(), n);
+    }
+    catch (std::exception e)
+    {
+      std::cerr << e.what();
+      Response res = Response::plaintext(400, "Bad Request");
+      job.first.write(res.serialize());
+      job.first.close();
+      return;
+    }
+
+    // Handle The Request
+    req.client_ip = job.second.to_string();
+    for (auto def : _route_definitions)
+    {
+      auto match = Internals::Router::match_request(def, req);
+      if (match.has_value())
+      {
+        try
+        {
+          req.url_params = match.value();
+          Response resp = def.handler(req);
+          job.first.write(resp.serialize());
+          job.first.close();
+          return;
+        }
+        catch (std::exception e)
+        {
           std::cerr << e.what();
-          Response res = Response::plaintext(400, "Bad Request");
+          Response res = Response::plaintext(500, "Server Internal Error");
           job.first.write(res.serialize());
           job.first.close();
           return;
         }
+      }
+    }
 
-        // Handle The Request
-        req.client_ip = job.second.to_string();
-        for (auto def : _route_definitions) {
-          auto match = Internals::Router::match_request(def, req);
-          if (match.has_value()) {
-            try {
-              req.url_params = match.value();
-              Response resp = def.handler(req);
-              job.first.write(resp.serialize());
-              job.first.close();
-              return;
-            } catch (std::exception e) {
-              std::cerr << e.what();
-              Response res = Response::plaintext(500, "Server Internal Error");
-              job.first.write(res.serialize());
-              job.first.close();
-              return;
-            }
-          }
-        }
-
-        Response res = Response::plaintext(404, "Page not found");
-        job.first.write(res.serialize());
-        job.first.close();
-        return;
-      };
+    Response res = Response::plaintext(404, "Page not found");
+    job.first.write(res.serialize());
+    job.first.close();
+    return;
+  };
 
   _pool = new ThreadPool(tcpClientHandler);
 }
 
-Server::~Server() {
+Server::~Server()
+{
   _pool->stop();
   delete _pool;
 }
 
-void Server::start(int thread_count) {
+void Server::start(int thread_count)
+{
   std::cout << "Server starting up...\n";
   _pool->start(thread_count);
 
-  while (true) {
+  while (true)
+  {
     sockpp::inet_address client_addr;
     sockpp::tcp_socket c_sock = _acceptor.accept(&client_addr);
 
-    if (!c_sock) {
+    if (!c_sock)
+    {
       std::cerr << "Error accepting connections\n";
       std::cerr << _acceptor.last_error_str() << "\n";
-    } else {
+    }
+    else
+    {
       _pool->queue_job(std::make_pair(std::move(c_sock), client_addr));
     }
   }
 }
 
 void Server::_route(std::string method, std::string route_template,
-                    RouteHandler func) {
+                    RouteHandler func)
+{
   auto def = Internals::Router::route_str_to_definition(route_template);
   def.method = method;
   def.handler = func;
   _route_definitions.push_back(def);
 }
-void Server::any(std::string route_template, RouteHandler func) {
+void Server::any(std::string route_template, RouteHandler func)
+{
   _route("ANY", route_template, func);
 }
-void Server::get(std::string route_template, RouteHandler func) {
+void Server::get(std::string route_template, RouteHandler func)
+{
   _route("GET", route_template, func);
 }
-void Server::post(std::string route_template, RouteHandler func) {
+void Server::post(std::string route_template, RouteHandler func)
+{
   _route("POST", route_template, func);
 }
-void Server::put(std::string route_template, RouteHandler func) {
+void Server::put(std::string route_template, RouteHandler func)
+{
   _route("PUT", route_template, func);
 }
-void Server::Delete(std::string route_template, RouteHandler func) {
+void Server::Delete(std::string route_template, RouteHandler func)
+{
   _route("DELETE", route_template, func);
+}
+void Server::patch(std::string route_template, RouteHandler func)
+{
+  _route("PATCH", route_template, func);
+}
+
+void Server::head(std::string route_template, RouteHandler func)
+{
+  _route("HEAD", route_template, func);
+}
+
+void Server::options(std::string route_template, RouteHandler func)
+{
+  _route("OPTIONS", route_template, func);
+}
+
+void Server::trace(std::string route_template, RouteHandler func)
+{
+  _route("TRACE", route_template, func);
 }
 
 // ThreadPool
@@ -126,14 +170,17 @@ ThreadPool::ThreadPool(
         runner_function)
     : runner(runner_function) {}
 
-void ThreadPool::start(unsigned int num_threads) {
-  for (unsigned int i = 0; i < num_threads; i++) {
+void ThreadPool::start(unsigned int num_threads)
+{
+  for (unsigned int i = 0; i < num_threads; i++)
+  {
     threads.emplace_back(&ThreadPool::_thread_loop, this);
   }
 }
 
 void ThreadPool::queue_job(
-    std::pair<sockpp::tcp_socket, sockpp::inet_address> job) {
+    std::pair<sockpp::tcp_socket, sockpp::inet_address> job)
+{
   {
     std::unique_lock<std::mutex> lock(queue_mutex);
     jobs.push(std::move(job));
@@ -142,14 +189,18 @@ void ThreadPool::queue_job(
   mutex_condition.notify_one();
 }
 
-void ThreadPool::_thread_loop() {
-  while (1) {
+void ThreadPool::_thread_loop()
+{
+  while (1)
+  {
     std::pair<sockpp::tcp_socket, sockpp::inet_address> job;
     {
       std::unique_lock<std::mutex> lock(queue_mutex);
       mutex_condition.wait(
-          lock, [this]() { return !jobs.empty() || should_terminate; });
-      if (should_terminate) {
+          lock, [this]()
+          { return !jobs.empty() || should_terminate; });
+      if (should_terminate)
+      {
         return;
       }
       job = std::move(jobs.front());
@@ -159,7 +210,8 @@ void ThreadPool::_thread_loop() {
   }
 }
 
-bool ThreadPool::is_busy() {
+bool ThreadPool::is_busy()
+{
   bool poolbusy;
   {
     std::unique_lock<std::mutex> lock(queue_mutex);
@@ -168,7 +220,8 @@ bool ThreadPool::is_busy() {
   return poolbusy;
 }
 
-void ThreadPool::stop() {
+void ThreadPool::stop()
+{
   if (should_terminate)
     return;
   {
@@ -176,7 +229,8 @@ void ThreadPool::stop() {
     should_terminate = true;
   }
   mutex_condition.notify_all();
-  for (std::thread &active_thread : threads) {
+  for (std::thread &active_thread : threads)
+  {
     active_thread.join();
   }
   threads.clear();
